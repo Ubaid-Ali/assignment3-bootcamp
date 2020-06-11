0b0d6945ac956ad5ac0e478d3b3e708cdaf35a99
Ubaid Ali (locally) <syedubaidali2020@gmail.com>
2020-06-12 00:32:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App(props) {
       <hr />
       <Dinner dish="Tikka Boti" sweetDish="Gajar ka Halwa" />
       <hr />
-      <Dinner dish="Chicken Karahi" sweetDish="Chocolates" />
+      <Dinner dish="Chicken Karahi" sweetDish="Rasgullay" />
     </div>
   );
 }
